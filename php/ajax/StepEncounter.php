1211e17b3db6d6161c47462c5d1a2ca1d65d8b26
nmalacarne <nicholas.malacarne@gmail.com>
2014-05-17 17:23:43 -0400

    - create script './php/ajax/StepEncounter.php'; used to step the encounter one turn

---
+++ php/ajax/StepEncounter.php
@@ -0,0 +1,41 @@
+<?php
+/**********************************INCLUDE*********************************** *
+* **************************************************************************** */
+include_once( __DIR__ . '/../encountermanager/EncounterManager.php' );
+
+/**
+ * RUN: http://localhost/encountermanager/php/ajax/StepEncounter.php
+ *************************************************************************** *
+ * Example (JavaScript/JQuery):
+ * ************************************************************************** *
+ *
+ *************************************************************************** *
+ * @param
+ *************************************************************************** *
+ * @return 
+ * On Success: 		
+ * On Exception: 	JSON	- file, line, and message of the exception
+ *************************************************************************** *
+ * @author(s) nicholas malacarne <nicholas.malacarne@gmail.com>
+ */
+try {
+
+    $ENC_MGR = EncounterManager::getInstance();
+    
+    $ENC_MGR->step();
+    
+    echo $ENC_MGR->getJSON();
+    
+}catch( Exception $e ){
+    
+    $report = array();
+    
+    $report[ 'file' ] 		= $e->getFile();
+    $report[ 'line' ] 		= $e->getLine();
+    $report[ 'message' ] 	= $e->getMessage();
+    
+	// return exception report as JSON -nm
+    echo json_encode( $report );
+}
+
+exit();
