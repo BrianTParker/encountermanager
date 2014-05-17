eae61f6c5bab471acea4647de1c61ad280e74aae
nmalacarne <nicholas.malacarne@gmail.com>
2014-05-17 16:35:07 -0400

    - added attributes to './php/actor/Actor.php' - added constructor to './php/actor/Actor.php'

--- php/actor/Actor.php
+++ php/actor/Actor.php
@@ -1,7 +1,7 @@
 <?php
 /**********************************INCLUDE*********************************** *
 * **************************************************************************** */
-
+include_once( __DIR__ . '/../dice/TwentySideDie.php' );
 
 /**
  *
@@ -14,5 +14,23 @@
  * @author(s) nicholas malacarne <nicholas.malacarne@gmail.com>
  */
 class Actor {
-    //put your code here
+    
+    // id for the actor -nm
+    private $id;
+    
+    // modifier for initiative roll -nm
+    private $mod;
+    
+    // dice roll value -nm
+    private $roll;
+    
+    function __construct($aId, $aMod) {
+        
+        // set id and mod values -nm
+        $this->id = $aId;
+        $this->mod = $aMod;
+        
+        // get random die roll value -nm
+        $this->roll = TwentySideDie::roll();
+    }
 }
